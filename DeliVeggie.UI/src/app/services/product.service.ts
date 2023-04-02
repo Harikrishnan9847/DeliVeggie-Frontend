237db60a78b3d9e74cbc80ee8322e7d7380f6fa0
Harikrishnan S <harikrishnan.s@pitsolutions.com>
2023-04-02 17:44:09 +0530

    Added dockerfile
    Added docker support

--- DeliVeggie.UI/src/app/services/product.service.ts
+++ DeliVeggie.UI/src/app/services/product.service.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http'
 })
 export class ProductService {
 
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = 'http://localhost:44350/api/';
   constructor(private http: HttpClient) { }
 
   getAll() : Observable<Product[]>
